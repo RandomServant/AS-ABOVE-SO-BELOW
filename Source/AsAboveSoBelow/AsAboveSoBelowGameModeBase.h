2e33ed6794de725f98c2e5924a7010e2ec2176d2
RandomServant <89282619+RandomServant@users.noreply.github.com>
2022-08-26 14:44:07 +1000

    Start

---
+++ Source/AsAboveSoBelow/AsAboveSoBelowGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AsAboveSoBelowGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASABOVESOBELOW_API AAsAboveSoBelowGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
